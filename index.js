21d3be033b341e7485b321e280490a9ae8cf52db
VinothkumarRBE-work <vinothkumarrbe.99@gmail.com>
2025-01-12 11:48:36 +0530

    Update  Updated MongoDB Connection index.js

--- index.js
+++ index.js
@@ -23,9 +23,14 @@ app.use(cors());
 // app.use(bodyParser.urlencoded({ extended: true })); 
  
 
+// // MongoDB connection
+// mongoose
+//   .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+//   .then(() => console.log("Connected to MongoDB"))
+//   .catch((err) => console.error("MongoDB connection error:", err));
+
 // MongoDB connection
-mongoose
-  .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("MongoDB connection error:", err));
 
